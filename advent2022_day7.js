19a0e109091633af6fecf8cbd8cdcd60f03881c0
Josh <facehair4000@gmail.com>
2022-12-08 08:40:59 -0500

    partial commit - now appending sizes

--- advent2022_day7.js
+++ advent2022_day7.js
@@ -23,34 +23,55 @@ $ ls
 5626152 d.ext
 7214296 k`;
 
+// helper function to find numbers
+function isNumeric(str) {
+  if (typeof str != "string") return false; // we only process strings!
+  return (
+    !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+    !isNaN(parseFloat(str))
+  ); // ...and ensure strings of whitespace fail
+}
+
 const buildFileStructure = (input) => {
   const outputLines = input.split("\n");
   const fileStructure = { home: {} };
   const depthTracker = [];
   let currentDir = "home";
   outputLines.forEach((line) => {
-    const isCommand = line[0] === "$";
+    const lineSplit = line.split(" ");
     // console.log(line);
-    if (isCommand) {
+    if (lineSplit[0] === "$") {
       const lineSplit = line.split(" ");
       const command = lineSplit[1];
       const input = lineSplit[2];
 
       switch (command) {
         case "cd":
-          console.log(line);
           if (input === "..") {
             currentDir = depthTracker[depthTracker.length - 1];
             depthTracker.splice(depthTracker.length - 1, 1);
           } else {
             depthTracker.push(currentDir);
             currentDir = input;
+            fileStructure[currentDir] = {};
           }
         case "ls":
-          fileStructure[]
+          // idk - i don't think I actually need to do anything with this.
+          break;
       }
+    } else if (isNumeric(lineSplit[0])) {
+      fileStructure[currentDir]["size"] = fileStructure[currentDir]["size"]
+        ? (fileStructure[currentDir]["size"] += parseInt(lineSplit[0], 10))
+        : parseInt(lineSplit[0], 10);
+
+      depthTracker.forEach((prevDir) => {
+        fileStructure[prevDir]["size"] = fileStructure[prevDir]["size"]
+          ? (fileStructure[prevDir]["size"] += parseInt(lineSplit[0], 10))
+          : parseInt(lineSplit[0], 10);
+      });
     }
   });
+  console.log(fileStructure);
   return fileStructure;
 };
 
